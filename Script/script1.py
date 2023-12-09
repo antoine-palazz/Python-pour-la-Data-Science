03e79b2fcfea3a38960cc35de8dbdc17409f07fa
vsmague <valentin.smague@ensae.fr>
2023-12-09 16:41:35 +0000

    playlist

--- Script/script1.py
+++ Script/script1.py
@@ -1,4 +1,14 @@
-print("bonjour")
-def f(x):
-    return x+2
-print(f(1))
+from collections import OrderedDict
+path = '/home/onyxia/work/Python-pour-la-Data-Science/playlist.txt'
+fichier = open(path, "r")
+playlist = fichier.read()
+fichier.close()
+playlist= playlist.split("\n")
+playlist = list(OrderedDict.fromkeys(playlist))
+nb_titres = len(playlist)
+for k in range(nb_titres):
+    song = playlist[k]
+    ind = 0
+    size = len(song)
+    while ind < size and song[ind] != "-":
+        ind += 1
