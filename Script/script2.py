e02d6dd623777ed12a4dfdfbc10dcb7079c40410
cgadeau <clement.gadeau@ensae.fr>
2023-12-09 15:58:16 +0000

    track request

--- Script/script2.py
+++ Script/script2.py
@@ -40,14 +40,53 @@ def access_token():
 token = access_token()
 
 
+# En-tête de la requête avec le jeton d'accès
+headers = {
+    'Authorization': f'Bearer {token}',
+}
 
 
-# ID Spotify du groupe Daft Punk
-artist_id = "4tZwfgrHOc3mvqYlEYSvVi"  
-# C'est l'ID de Daft Punk, vous pouvez obtenir l'ID d'un artiste en recherchant sur l'API Spotify.
+def get_track_id(track):
+    
+    # Request for the track
+    search_url = "https://api.spotify.com/v1/search"
+    search_params = {
+        'q': track,
+        'type': 'track',
+        'limit': 1,
+    }
 
-# URL de l'endpoint pour obtenir des informations sur un artiste
-artist_url = f"https://api.spotify.com/v1/artists/{artist_id}"
+    # Search
+    search_response = requests.get(search_url, params=search_params, headers=headers)
+    search_data = search_response.json()
+
+    # Verify if there are results
+    if 'tracks' in search_data and 'items' in search_data['tracks'] and search_data['tracks']['items']:
+        # Récupérer l'ID de la première chanson trouvée
+        track_id = search_data['tracks']['items'][0]['id']
+
+        # Récupérer les caractéristiques de la piste
+        features_url = f"https://api.spotify.com/v1/audio-features/{track_id}"
+        features_response = requests.get(features_url, headers=headers)
+        features_data = features_response.json()
+
+        # Vérifier si des résultats ont été trouvés pour les caractéristiques de la piste
+        if features_data:
+            print("Caractéristiques de la piste 'Verona' de Muse :")
+            print(f"Clé: {features_data['key']}")
+            print(f"Tempo: {features_data['tempo']}")
+            print(f"Énergie: {features_data['energy']}")
+            # Ajoutez d'autres caractéristiques selon vos besoins
+        else:
+            print("Aucun résultat trouvé pour les caractéristiques de la piste 'Verona' de Muse")
+    else:
+        print("Aucun résultat trouvé pour la chanson 'Verona' de Muse")
+
+
+
+
+# URL de l'endpoint pour obtenir les caractéristiques de la piste
+features_url = f"https://api.spotify.com/v1/audio-features/{track_id}"
 
 # En-tête de la requête avec le jeton d'accès
 headers = {
@@ -55,16 +94,44 @@ headers = {
 }
 
 # Effectuer la requête GET
-response = requests.get(artist_url, headers=headers)
+response = requests.get(features_url, headers=headers)
 
 # Vérifier la réponse
 if response.status_code == 200:
-    # La réponse est au format JSON, vous pouvez extraire les informations de l'artiste de cette façon
-    artist_info = response.json()
-    print(f"Informations sur l'artiste Daft Punk :")
-    print(f"Nom: {artist_info['name']}")
-    print(f"Genres: {', '.join(artist_info['genres'])}")
-    print(f"Popularité: {artist_info['popularity']}")
-    print(f"URL Spotify: {artist_info['external_urls']['spotify']}")
+    # La réponse est au format JSON, imprimez toutes les caractéristiques
+    features_data = response.json()
+    print("Caractéristiques de la piste :")
+    for key, value in features_data.items():
+        print(f"{key}: {value}")
+else:
+    print(f"Erreur lors de la requête : {response.status_code} - {response.text}")
+
+
+
+
+
+
+
+# URL de l'endpoint pour obtenir les caractéristiques de la piste
+analysis_url = f"https://api.spotify.com/v1/audio-analysis/{track_id}"
+
+# En-tête de la requête avec le jeton d'accès
+headers = {
+    'Authorization': f'Bearer {token}',
+}
+
+# Effectuer la requête GET
+response = requests.get(analysis_url, headers=headers)
+
+# Vérifier la réponse
+if response.status_code == 200:
+    # La réponse est au format JSON, imprimez toutes les caractéristiques
+    analysis_data = response.json()
+    print("Caractéristiques de la piste :")
+    i = 0
+    for key, value in analysis_data.items():
+        print(f"{key}: {value}", i, "\n \n \n")
+        i += 1
+    print(len(analysis_data.items()))
 else:
     print(f"Erreur lors de la requête : {response.status_code} - {response.text}")
