58943e1b410e47c9986e6a8ee723c52def4de540
vsmague <valentin.smague@ensae.fr>
2023-12-27 14:02:56 +0000

    genre avec artiste

---
+++ Script/script5.py
@@ -0,0 +1,28 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Remplacez ces valeurs par votre Client ID et Client Secret
+client_id = "b6871ef2f14a4b60888a32454862b876"
+client_secret = "3a5d22aa09434e32a9760159ca3bb234"
+
+# Initialisez l'authentification client
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# Fonction pour obtenir les genres d'un artiste
+def get_artist_genres(artist_name):
+    results = sp.search(q='artist:' + artist_name, type='artist')
+    if results['artists']['items']:
+        artist = results['artists']['items'][0]
+        genres = artist['genres']
+        return genres
+    else:
+        return None
+
+# Exemple d'utilisation
+artist_name = 'Bob Marley'
+genres = get_artist_genres(artist_name)
+if genres:
+    print(f"Genres de l'artiste {artist_name}: {', '.join(genres)}")
+else:
+    print(f"Aucun résultat trouvé pour l'artiste {artist_name}")
