62f093a355d7eed43182269f293f01b663816a30
vsmague <valentin.smague@ensae.fr>
2023-12-28 18:05:02 +0000

    mm

--- Script/modifierdatatabase.py
+++ Script/modifierdatatabase.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import ast
+
 Titles_en_csv = '/home/onyxia/work/Python-pour-la-Data-Science/Data/data/Titles2.csv'
 
+
+'''
 # Lire le fichier CSV avec des options supplémentaires
 Titles = pd.read_csv(Titles_en_csv, delimiter=',')
 Titles = Titles.drop(['type','uri','id','track_href','analysis_url','artist','artist_id','genres','Title','track_id'], axis = 1)
 
 path = '/home/onyxia/work/Python-pour-la-Data-Science/Data/data/Titles3.csv'
 Titles.to_csv(path, index=False)
-#%%
+
+
+
+
+
+
 
 #Matrice des corrélations
 import pandas as pd
@@ -34,3 +43,5 @@ variance_per_column = Titles.var()
 # Affichage de la variance des colonnes
 print("\nVariance des colonnes:")
 print(variance_per_column)
+
+'''
