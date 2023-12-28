aff0f42d86820cdfccdc8b26e6dd8e6d260e3e90
vsmague <valentin.smague@ensae.fr>
2023-12-28 15:02:21 +0000

    ???

--- Script/run.py
+++ Script/run.py
@@ -23,14 +23,14 @@ for k in range(nb_utilisation_token_track-1):
 track_features_list = track_features_list + get_track_features(track_list[(nb_utilisation_token_track-1)*100:],headers)
 Titles['track_features'] = track_features_list
 
-'''
+
 Features = get_features_labels(headers)
 #print(Features)
 
 for feature in Features:
     Titles[feature] = Titles['track_features'].apply(lambda x: x.get(feature))
 Titles.drop(columns=['track_features'], inplace = True)
-'''
+
 artist_list = Titles['artist_id'].tolist()
 nb_artist = len(artist_list)
 print("il y a", nb_artist, "artistes")
@@ -40,12 +40,11 @@ artist_genres_list = []
 for k in range(nb_utilisation_token_artist-1):
     artist_genres_list = artist_genres_list + get_artists_genres(artist_list[50*k:50*(k+1)], headers)
     print(k/nb_utilisation_token_artist)
-artist_genres_list = artist_genres_list + get_artists_genres(artist_list[(nb_utilisation_token_track-1)*50:], headers)
+artist_genres_list = artist_genres_list + get_artists_genres(artist_list[(nb_utilisation_token_artist-1)*50:], headers)
 Titles['genres'] = artist_genres_list
 
 print(Titles.head())
 
-'''
+
 path = '/home/onyxia/work/Python-pour-la-Data-Science/Data/data/Titles2.csv'
 Titles.to_csv(path, index=False)
-'''
