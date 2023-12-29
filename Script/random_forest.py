c8232b55246399466f3ea74decea91e49c84438a
vsmague <valentin.smague@ensae.fr>
2023-12-29 16:04:11 +0000

    ml ok

---
+++ Script/random_forest.py
@@ -0,0 +1,127 @@
+#%%
+import pandas as pd # Pour le dataframe
+import numpy as np # Pour la normalisation et calculs de moyenne
+import matplotlib.pyplot as plt # Pour la visualisation
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.model_selection import train_test_split, GridSearchCV, validation_curve, RandomizedSearchCV # Split de dataset et optimisation des hyperparamètres
+from sklearn.ensemble import RandomForestClassifier # Random forest
+from sklearn.ensemble import GradientBoostingClassifier # XGBoost
+from sklearn.neighbors import KNeighborsClassifier # k-NN
+from sklearn.svm import SVC # SVM
+from sklearn.metrics import accuracy_score, confusion_matrix, recall_score, f1_score, zero_one_loss, classification_report # Métriques pour la mesure de performances
+from sklearn.preprocessing import normalize, StandardScaler
+import seaborn as sns
+
+df_str = '/home/onyxia/work/Python-pour-la-Data-Science/Data/data/df.csv'
+df = pd.read_csv(df_str, delimiter=',')
+df = df.drop(['type','uri','id','track_href','analysis_url','artist','artist_id','Title','track_id'], axis = 1)
+features = df
+# valeurs à prédire
+genres = np.array(features['genre'])
+# supprime les labels des données
+features = features.drop('genre', axis = 1)
+# sauvegarde le nom de features
+feature_list = list(features.columns)
+# conversion en numpy array
+features = np.array(features)
+
+# séparer les données en training and testing sets
+train_features, test_features, train_genres, test_genres = train_test_split(features, genres, test_size = 0.25, random_state = 0, shuffle = True)
+print('Training Features Shape:', train_features.shape)
+print('Training Genres Shape:', train_genres.shape)
+print('Testing Features Shape:', test_features.shape)
+print('Testing Genres Shape:', test_genres.shape)
+
+sc = StandardScaler()
+train_features = sc.fit_transform(train_features)
+test_features = sc.transform(test_features)
+
+rf = RandomForestClassifier(random_state = 0)
+from pprint import pprint
+print('Parameters currently in use:\n')
+pprint(rf.get_params())
+
+# nombre d'arbres
+n_estimators = [500, 1000, 2000, 3000, 4000, 5000]
+# profondeur max de l'arbre
+max_depth = [20]
+max_depth.append(None)
+# nombre d'échantillon min nécessaire par noeuds
+min_samples_split = [2, 4]#[2]
+# nombre d'échantillon min nécessaire par feuilles
+min_samples_leaf = [1, 2]#[1]
+
+# création de la grille
+random_grid = {'n_estimators': n_estimators,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+              }
+pprint(random_grid)
+
+# création du modèle
+rf = RandomForestClassifier(random_state = 0, max_features = 'sqrt', bootstrap = True)
+
+# random search
+rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 10, cv = 3, verbose=2, random_state=0, n_jobs = -1)
+
+# fit le modèle
+rf_random.fit(train_features, train_genres)
+
+pd_res = pd.concat([pd.DataFrame(rf_random.cv_results_["params"]),pd.DataFrame(rf_random.cv_results_["mean_test_score"], columns=["Accuracy"])],axis=1)
+pd_res = pd_res.sort_values('Accuracy', ascending=False)
+print(rf_random.best_params_)
+print(pd_res)
+
+param_grid = {
+    'max_depth': [20, None],
+    'min_samples_split': [4],
+    'n_estimators': [2000, 4000]
+}
+pprint(param_grid)
+
+# création du modèle
+rf = RandomForestClassifier(random_state = 0, bootstrap=True)
+
+# grid search
+grid_search = GridSearchCV(estimator = rf, param_grid = param_grid, cv = 3, n_jobs = -1, verbose = 2)
+
+grid_search.fit(train_features, train_genres)
+
+pd_res = pd.concat([pd.DataFrame(grid_search.cv_results_["params"]),pd.DataFrame(grid_search.cv_results_["mean_test_score"], columns=["Accuracy"])],axis=1)
+pd_res = pd_res.sort_values('Accuracy', ascending=False)
+print(pd_res)
+
+# création du modèle
+rf = RandomForestClassifier(n_estimators=4000, max_features='sqrt', max_depth=20, min_samples_split=4, min_samples_leaf=1, bootstrap=True, criterion='gini' ,random_state=0)
+
+# fit le modèle
+rf.fit(train_features, train_genres)
+
+# prédictions
+predictions = rf.predict(test_features)
+
+# Zero_one_loss error
+errors = zero_one_loss(test_genres, predictions, normalize=True)
+print('zero_one_loss error normalized:', errors)
+
+# Accuracy Score
+accuracy_test = accuracy_score(test_genres, predictions)
+print('accuracy_score on test dataset :', accuracy_test)
+
+print(classification_report(predictions, test_genres))
+
+sns.set()
+mat = confusion_matrix(test_genres, predictions)
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False, xticklabels=genres, yticklabels=genres)
+plt.xlabel('true genre')
+plt.ylabel('predicted genre')
+
+plt.style.use('fivethirtyeight')
+
+importances = list(rf.feature_importances_)
+
+x_values = list(range(len(importances)))
+plt.bar(x_values, importances, orientation = 'vertical')
+plt.xticks(x_values, feature_list, rotation='vertical')
+plt.ylabel('Importance'); plt.xlabel('Variable'); plt.title('Variable Importances')
